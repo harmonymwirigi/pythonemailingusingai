e337274e02ad533c64c37692876243ca90323950
faith993 <chepkemoitoo3@gmail.com>
2024-03-27 21:48:13 +0300

    dashboad customization

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from auth.route import auth_bp
 from user.route import user_bp
-from admin.route import admin_bp
-from auth.model import db
-from flask_admin import Admin, admin
+from mails.model import Mails
+from flask_admin.contrib.sqla import ModelView
+
+from auth.model import db,User
+from flask_admin import Admin
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -15,11 +18,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 csrf = CSRFProtect(app)
   # Initialize Flask-Admin with your app
+admin = Admin()
 
+# Register the blueprint with your Flask application
+admin.init_app(app)
 # Register Blueprints
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(user_bp, url_prefix='/user')
-app.register_blueprint(admin_bp, url_prefix='/admin')
+# Register Flask-Admin views for your models
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Mails, db.session))
+
 
 if __name__ == "__main__":
     with app.app_context():
